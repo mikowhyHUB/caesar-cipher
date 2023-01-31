3f3c26c4e036134ee2235214ef7ed5c83e76d21b
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2023-01-31 12:26:15 +0100

    feat: added buffer methods

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import string
 
 class Cipher:
 
-    def encrypt(self, plain_text: str,
-                n) -> str:  # todo: lepiej zrobic dwa encrypty/decrypty i input w srodku. bardziej elastyczna metoda
+    def encrypt(self) -> str:
+        ROT13 = 13
         encrypted = ""
+        plain_text = input('test encrypt: ')
         for i in range(len(plain_text)):
             if plain_text[i] == " ":
                 encrypted += " "
             elif plain_text[i].isupper():
-                encrypted += chr((ord(plain_text[i]) + n - 65) % 26 + 65)
+                encrypted += chr((ord(plain_text[i]) + ROT13 - 65) % 26 + 65)
             else:
-                encrypted += chr((ord(plain_text[i]) + n - 97) % 26 + 97)
+                encrypted += chr((ord(plain_text[i]) + ROT13 - 97) % 26 + 97)
         return encrypted
 
     def decrypt(self, cipher_text: str) -> str:
@@ -36,30 +37,39 @@ class FileHandler:
 
 
 class Buffer:
-    pass
+    def __init__(self):
+        self.cipher = Cipher()
+        self.menu = Menu()
+        self.menu_options = {1: self.cipher.encrypt, 2: 'test'}
+        self.file_options = {1: '...', 2: '...'}
+
+    def execute_choice(self):
+        print(self.menu_options.get(self.menu.show_menu())())
 
 
 class Menu:
-    def __init__(self):
-        self.cipher = Cipher
-        self.menu_options = {1: self.cipher.encrypt(), 2: 'test'}
 
     def show_menu(self):
-        pass
+        return int(input('Options 1,2,3: '))
 
-    def options_with_file(self):
-        pass
+    def show_file_options(self):
+        return int('What would you like to do with encrypted text: 1, 2, 3...')
 
 
-class Manager:
-    pass
+class Manager(Buffer):
+
+    def start(self):
+        Buffer().execute_choice()
 
 
 def main():
-    test = Cipher()
-    print(test.encrypt('ala ma kota'))
-    print(test.decrypt('Nyn Zn xbgn'))
+    manager = Manager()
+    manager.start()
 
 
 if __name__ == '__main__':
     main()
+
+'''Pytania:
+1. dlaczego w menu_options jak zrobię do metody () to się z automatu wywołuje nawet jak jej nie calluje
+2. Po co dziedziczyć skoro można w __init__ zrobić atrybut jako dana klasa i mamy te same metody dostępne '''
