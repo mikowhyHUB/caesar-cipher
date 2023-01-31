67fe810423598b954d1d4f45b5102b200abdbb51
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2023-01-31 10:01:17 +0100

    feat: added new methods to Cipher

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+from dataclasses import dataclass
+import string
+
+
 class Cipher:
-    pass
+
+    def encrypt(self, plain_text: str) -> str:
+        n = 13
+        encrypted = ""
+        for i in range(len(plain_text)):
+            if plain_text[i] == " ":
+                encrypted += " "
+            elif plain_text[i].isupper():
+                encrypted += chr((ord(plain_text[i]) + n - 65) % 26 + 65)
+            else:
+                encrypted += chr((ord(plain_text[i]) + n - 97) % 26 + 97)
+        return encrypted
+
+    def decrypt(self, cipher_text: str) -> str:
+        alphabet = string.ascii_lowercase
+        n = 13
+        decrypted_message = ""
+        for char in cipher_text.lower():
+            if char in alphabet:
+                position = alphabet.find(char)
+                new_pos = (position - n) % 26
+                new_char = alphabet[new_pos]
+                decrypted_message += new_char
+            else:
+                decrypted_message += char
+        return decrypted_message
 
 
 class FileHandler:
@@ -10,9 +39,24 @@ class Buffer:
     pass
 
 
+@dataclass
 class Menu:
     pass
+    # menu_options: dict = {1: '...', 2: '...', 3: '...'}
+    #
+    # def show(self):
+    #     pass
 
 
 class Manager:
     pass
+
+
+def main():
+    test = Cipher()
+    print(test.encrypt('ala ma kota'))
+    print(test.decrypt('Nyn Zn xbgn'))
+
+
+if __name__ == '__main__':
+    main()
