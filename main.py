5cbff51d4a68372214160249e396950261b7ca9c
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2023-01-31 18:46:25 +0100

    feat: working with functionality of menu

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ class Cipher:
     def __init__(self):
         self.test_encrypted = []
 
-    def encrypt(self) -> None:
+    def encrypt_rot13(self) -> None:
         ROT13 = 13
         encrypted = ""
-        plain_text = input('test encrypt: ')
+        plain_text = input(
+            'test encrypt_rot13: ')  # mozliwe, ze bd musial to zmienic i dodawac w self bo koliduje z zapisywaniem plikow
         for i in range(len(plain_text)):
             if plain_text[i] == " ":
                 encrypted += " "
@@ -19,22 +20,48 @@ class Cipher:
                 encrypted += chr((ord(plain_text[i]) + ROT13 - 97) % 26 + 97)
         return self.test_encrypted.append(encrypted)
 
-    def decrypt(self, cipher_text: str) -> str:
+    def encrypt_rot47(self) -> None:
+        ROT47 = 47
+        encrypted = ""
+        plain_text = input('test encrypt_rot47: ')
+        for i in range(len(plain_text)):
+            if plain_text[i] == " ":
+                encrypted += " "
+            elif plain_text[i].isupper():
+                encrypted += chr((ord(plain_text[i]) + ROT47 - 65) % 26 + 65)
+            else:
+                encrypted += chr((ord(plain_text[i]) + ROT47 - 97) % 26 + 97)
+        return self.test_encrypted.append(encrypted)
+
+    def decrypt_rot13(self) -> str:
         alphabet = string.ascii_lowercase
-        n = 13
+        ROT13 = 13
         decrypted = ""
+        cipher_text = input('test decrypt_rot13: ')
         for char in cipher_text.lower():
             if char in alphabet:
                 position = alphabet.find(char)
-                new_pos = (position - n) % 26
+                new_pos = (position - ROT13) % 26
                 new_char = alphabet[new_pos]
                 decrypted += new_char
             else:
                 decrypted += char
         return decrypted
 
-    def printing(self):
-        print(self.test_encrypted)
+    def decrypt_rot47(self) -> str:
+        alphabet = string.ascii_lowercase
+        ROT47 = 47
+        decrypted = ""
+        cipher_text = input('test decrypt_rot47: ')
+        for char in cipher_text.lower():
+            if char in alphabet:
+                position = alphabet.find(char)
+                new_pos = (position - ROT47) % 26
+                new_char = alphabet[new_pos]
+                decrypted += new_char
+            else:
+                decrypted += char
+        return decrypted
 
 
 class FileHandler:
@@ -45,14 +72,14 @@ class Buffer:
     def __init__(self):
         self.cipher = Cipher()
         self.menu = Menu()
-        self.menu_options = {1: self.cipher.encrypt, 2: self.cipher.decrypt}
-        self.file_options = {1: self.cipher.test_encrypted, 2: 'save to file'}
+        self.menu_dict = {1: self.cipher.encrypt_rot13, 2: self.cipher.decrypt_rot13}
+        self.encrypt_dict = {1: self.cipher.test_encrypted, 2: 'save to file'}
 
     def menu_choice(self, x):
-        return self.menu_options.get(x)()
+        return self.menu_dict.get(x)()
 
-    def file_option(self):
-        print(self.file_options.get(self.menu.show_file_options()))
+    def encrypt_options(self):
+        print(self.encrypt_dict.get(self.menu.show_encrypt_options()))
 
 
 class Menu:
@@ -61,10 +88,15 @@ class Menu:
         print('Welcome to Caesar Cipher')
 
     def show_menu(self):
-        return int(input('Options 1,2,3: '))
+        return int(input('Options 1: encrypt13, 2: encrypt47, 3: decrpt13, 4: decrypt14, 5: show files, 6: exit '))
 
-    def show_file_options(self):
-        return int(input('What would you like to do with encrypted text: 1print, 2, 3...'))
+    def show_encrypt_options(self):
+        return int(
+            input('What would you like to do with encrypted text: 1print, 2add another, 3save to file, 4.quit...'))
+
+    def show_decrypt_options(self):
+        return int(
+            input('What would you like to do with decrypted text: 1print, 2add another, 3save to file, 4.quit...'))
 
 
 class Manager:
@@ -80,7 +112,7 @@ class Manager:
         return x
 
     def options(self):
-        self.buffer.file_option()
+        self.buffer.encrypt_options()
 
 
 def main():
