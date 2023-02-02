ac8515273452053f138af8617745a2d319d3638c
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2023-02-02 17:33:07 +0100

    feature: testing pre-commit

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ def main():
             sleep(1)
             manager.additional_menu()
             sleep(1)
+            sadasdad
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
-'''Pytania:
+"""Pytania:
 1. dlaczego w menu_options jak zrobię do metody () to się z automatu wywołuje nawet jak jej nie calluje
 2. Po co dziedziczyć skoro można w __init__ zrobić atrybut jako dana klasa i mamy te same metody dostępne
 3. Czy tam gdzie pycharm mi podkresla by powinno byc staticmethod to robic staticmethod?
 4. odnośnie branchy. Jak zmerdżuje z mainem to usuwać branch?
 5. czy kolenosc metod w klasie jest wazna dla czytelnosci kodu?
-3. Jaki jest proces twórczy osoby doświadczonej. Co po kolei robi'''
+3. Jaki jest proces twórczy osoby doświadczonej. Co po kolei robi"""
+
+# todo: requirements.txt
