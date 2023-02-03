32467acd7dd6012b4ce61530ba9a8a026fe34921
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2023-02-03 09:44:12 +0100

    feat: added typing and docstring

--- filehandler.py
+++ filehandler.py
@@ -8,24 +8,24 @@ class FileHandler(Cipher):
 
     def __init__(self) -> None:
         super().__init__()
-        self.json_dict: dict = {}
+        self._json_dict: dict = {}
 
-    def add_name_to_cipher(self) -> None:
+    def name_the_cipher(self) -> None:
         """User can choose how his text should be named in JSON file"""
         name_text = input("How would you like to name this text: ")
-        self.json_dict[name_text] = buffer1.name
+        self._json_dict[name_text] = buffer1.name
 
-    def change_to_dict(self) -> None:
+    def prepare_dict(self) -> None:
         """Preparing Buffer object to dict"""
-        self.add_name_to_cipher()
-        self.json_dict["status"] = buffer1.status
-        self.json_dict["rot"] = buffer1.rot
+        self.name_the_cipher()
+        self._json_dict["status"] = buffer1.status
+        self._json_dict["rot"] = buffer1.rot
 
     def save_file(self) -> None:
         """Saving to JSON file"""
-        self.change_to_dict()
+        self.prepare_dict()
         with open("cipher.json", "w", encoding="utf-8") as cipher:
-            json.dump(self.json_dict, cipher, ensure_ascii=False, indent=4)
+            json.dump(self._json_dict, cipher, ensure_ascii=False, indent=4)
             print("File saved. Returning to main menu")
 
     def print_file(self) -> None:
