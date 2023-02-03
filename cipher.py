13dee70ad2c0de239fc6cfa6d6e3968eb783fa98
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2023-02-03 08:55:18 +0100

    feat: added typing and docstring

--- cipher.py
+++ cipher.py
@@ -3,14 +3,17 @@ from buffer import Buffer, buffer1
 
 
 class Cipher(Buffer):
+    """Class for cipher users text"""
+
     def __init__(self) -> None:
         super().__init__()
         self.ROT13 = 13
         self.ROT47 = 47
 
     def encrypt_rot13(self) -> None:
+        """Encrypting users text with ROT13 method"""
         encrypted = ""
-        plain_text = input('What text would you like to encrypt: ')
+        plain_text = input("What text would you like to encrypt: ")
         for i in range(len(plain_text)):
             if plain_text[i] == " ":
                 encrypted += " "
@@ -18,12 +21,12 @@ class Cipher(Buffer):
                 encrypted += chr((ord(plain_text[i]) + self.ROT13 - 65) % 26 + 65)
             else:
                 encrypted += chr((ord(plain_text[i]) + self.ROT13 - 97) % 26 + 97)
-        buffer1.name, buffer1.status, buffer1.rot = encrypted, 'encrypted', 'ROT13'
+        buffer1.name, buffer1.status, buffer1.rot = encrypted, "encrypted", "ROT13"
 
-    # te ponizej do update
-    def encrypt_rot47(self) -> dict:
+    def encrypt_rot47(self) -> None:
+        """Encrypting users text with ROT47 method"""
         encrypted = ""
-        plain_text = input('What text would you like to encrypt: ')
+        plain_text = input("What text would you like to encrypt: ")
         for i in range(len(plain_text)):
             if plain_text[i] == " ":
                 encrypted += " "
@@ -31,14 +34,13 @@ class Cipher(Buffer):
                 encrypted += chr((ord(plain_text[i]) + self.ROT47 - 65) % 26 + 65)
             else:
                 encrypted += chr((ord(plain_text[i]) + self.ROT47 - 97) % 26 + 97)
-        buffer1.name = encrypted
-        return buffer1.name
-        # self.cipher_text['name'], self.cipher_text['status'], self.cipher_text['rot'] = encrypted, 'encrypted', 'ROT47'
+        buffer1.name, buffer1.status, buffer1.rot = encrypted, "encrypted", "ROT47"
 
-    def decrypt_rot13(self) -> dict:
+    def decrypt_rot13(self) -> None:
+        """Decrypting users text with ROT13 method"""
         alphabet = string.ascii_lowercase
         decrypted = ""
-        cipher_text = input('What text would you like to decrypt: ')
+        cipher_text = input("What text would you like to decrypt: ")
         for char in cipher_text.lower():
             if char in alphabet:
                 position = alphabet.find(char)
@@ -47,13 +49,13 @@ class Cipher(Buffer):
                 decrypted += new_char
             else:
                 decrypted += char
-        self.cipher_text['name'], self.cipher_text['status'], self.cipher_text['rot'] = decrypted, 'decrypted', 'ROT13'
-        return self.cipher_text
+        buffer1.name, buffer1.status, buffer1.rot = decrypted, "decrypted", "ROT13"
 
-    def decrypt_rot47(self) -> dict:
+    def decrypt_rot47(self) -> None:
+        """Decrypting users text with ROT47 method"""
         alphabet = string.ascii_lowercase
         decrypted = ""
-        cipher_text = input('What text would you like to decrypt: ')
+        cipher_text = input("What text would you like to decrypt: ")
         for char in cipher_text.lower():
             if char in alphabet:
                 position = alphabet.find(char)
@@ -62,5 +64,4 @@ class Cipher(Buffer):
                 decrypted += new_char
             else:
                 decrypted += char
-        self.cipher_text['name'], self.cipher_text['status'], self.cipher_text['rot'] = decrypted, 'decrypted', 'ROT47'
-        return self.cipher_text
+        buffer1.name, buffer1.status, buffer1.rot = decrypted, "decrypted", "ROT47"
