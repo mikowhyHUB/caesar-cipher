fd59f48c2a7fe5c9cb3f7498137abe842208a048
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2023-02-20 10:24:34 +0100

    feat: changes in structure

--- src/buffer.py
+++ src/buffer.py
@@ -1,29 +1,26 @@
 from datetime import datetime
 
 
-
 class Buffer:
     """Class storing list of cipher text"""
 
     def __init__(self):
-        self.text = Text()
         self.memory: list = []
 
 
 class Text:
 
-    def __init__(self, name: str = None, status: str = None, rot: int = None) -> None:
+    def __init__(self, status: str = None, rot: int = None) -> None:
         """Text object"""
-        self.name = name
         self.status = status
         self.rot = rot
-        self.memory: list = []
         self.DT_STRING: str = str(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+        self.buffer = Buffer()
 
-    def to_dct(self, name: str, memory: str, status: str, rot: int) -> dict:
+    def to_dct(self, status: str, rot: int) -> dict:
         """Changing Text object to dict"""
         return {
-            name: memory,
+            'text': self.buffer.memory,
             "created": self.DT_STRING,
             "status": status,
             "rot": rot,
