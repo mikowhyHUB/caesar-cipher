b4e17eeefb2cfe55e1739a13b25fbb20649e3b3a
itsnotmikowhy <111461945+itsnotmikowhy@users.noreply.github.com>
2023-02-25 17:42:51 +0100

    feat: massive changes

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,51 +1,29 @@
+from unittest.mock import patch
+from io import StringIO
 from src.menu import Menu
-from unittest.mock import patch, call
 
 
-def test_is_intro_printing(capfd):
-    m = Menu()
-    m.intro()
-    captured = capfd.readouterr()
-    assert captured.out == "\nWelcome to Caesar Cipher\n\n"
+def test_user_menu_choice_main_menu(monkeypatch):
+    with patch("builtins.input", side_effect=["1"]):
+        menu = Menu(main_menu=True)
+        choice = menu.user_menu_choice()
+        assert choice == 1
 
 
-def test_show_menu_method():
-    with patch("builtins.input", return_value="1"):
-        assert Menu.show_menu() == 1
+#
+# def test_user_menu_choice_additional_options(monkeypatch, capsys):
+#     with patch("builtins.input", side_effect=["2"]):
+#         menu = Menu(main_menu=False)
+#         choice = menu.user_menu_choice()
+#         assert choice == 2
+#     captured = capsys.readouterr()
+#     assert Menu.ADDITIONAL_OPTIONS in captured.out
 
 
-def test_show_menu_invalid(monkeypatch):
-    user_input = '7'
-    with patch('builtins.input', side_effect=user_input):
-        assert Menu.show_menu() == 7
-
-
-def test_show_menu(monkeypatch):
-    monkeypatch.setattr('builtins.input', lambda _: 2)
-    i = Menu.show_menu()
-    assert i == 2
-
-
-@patch("builtins.print")
-def test_show_menu_methods(mocked_print):
-    with patch("builtins.input", return_value="1"):
-        Menu.show_menu()
-
-        assert mocked_print.mock_calls == [call(Menu.INTRO_TEXT)]
-
-
-def test_show_additional_options():
-    with patch("builtins.input", return_value="1"):
-        assert Menu.show_additional_options() == 1
-
-
-def test_show_additional_options_invalid(monkeypatch):
-    user_input = '7'
-    with patch('builtins.input', side_effect=user_input):
-        assert Menu.show_additional_options() == 7
-
-
-@patch("builtins.print")
-def test_show_additional_options_methods(mocked_print):
-    with patch("builtins.input", return_value="1"):
-        Menu.show_additional_options()
+def test_user_menu_choice_invalid_input(monkeypatch, capsys):
+    with patch("builtins.input", side_effect=["a", "b", "1"]):
+        menu = Menu(main_menu=True)
+        choice = menu.user_menu_choice()
+        assert choice == 1
+    captured = capsys.readouterr()
+    assert "You have entered invalid value. Try again." in captured.out
